f00322e51e18f50de84ca8d77beee33f87087df4
hanshanley <hwahhanley@gmail.com>
2023-02-26 12:37:09 -0800

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -199,7 +199,7 @@ def model_eval_test_multitask(sentiment_dataloader,
             b_mask2 = b_mask2.to(device)
 
             logits = model.predict_similarity(b_ids1, b_mask1, b_ids2, b_mask2)
-            y_hat = logits.sigmoid().flatten().cpu().numpy()
+            y_hat = logits.flatten().cpu().numpy()
 
             sts_y_pred.extend(y_hat)
             sts_sent_ids.extend(b_sent_ids)
