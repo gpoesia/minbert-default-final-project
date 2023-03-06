871aae21bc732bd49e0efed8702a5a8750e0060f
pvongchanh12 <106230870+pvongchanh12@users.noreply.github.com>
2023-03-05 20:33:14 -0800

    updates

--- optimizer.py
+++ optimizer.py
@@ -1,5 +1,6 @@
 from typing import Callable, Iterable, Tuple
 import math
+import numpy as np
 
 import torch
 from torch.optim import Optimizer
@@ -43,6 +44,7 @@ class AdamW(Optimizer):
                 state = self.state[p]
 
                 # Access hyperparameters from the `group` dictionary
+                # alpha is step size
                 alpha = group["lr"]
 
                 # Complete the implementation of AdamW here, reading and saving
@@ -59,28 +61,69 @@ class AdamW(Optimizer):
                 #    (incorporating the learning rate again).
 
                 ### TODO
-
-                #Lets first initalize our state if needed
+                # Initializing the state
                 if len(state) == 0:
+                    # Moving average of gradient values m_t
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    # Moving average of the squared gradient values v_t
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+                    # Time step
                     state['step'] = 0
-                    #we want exp moving average of gradient values
-                    state['exp_average'] = torch.zeros_like(p.data)
-                    #we also want moving average of the squared gradient values
-                    state['exp_average_sq'] = torch.zeros_like(p.data)
-                   
-                
-                exp_average, exp_average_sq = state['exp_average'], state['exp_average_sq']
 
-                
+
+                m_t, v_t, step = state['exp_avg'], state['exp_avg_sq'], state['step']
                 beta1, beta2 = group['betas']
 
-                state['step'] += 1 
 
-                # 1- Update first and second moments of the gradients
+                # We take one step for every theta
+                step += 1
 
+                # Calculating the moving averages
+                # Here, m_{t-1} and v_{t-1} will always be the previous calculation before the update for the first step, they're 0!
+                # We also don't need to account for W and b separately
+                m_t = beta1 * m_t + (1 - beta1) * grad
+                v_t = beta2 * v_t + (1 - beta2) * (grad*grad)
 
+                # Applying bias correction
+                alpha = alpha * (np.sqrt(1 - beta2)/(1 - beta1))
 
+                # Updating params less efficiently
+                # m_t_corrected = m_t/(1 - beta1)
+                # v_t_corrected = v_t/(1 - beta2)
+                # p.data -= alpha * m_t_corrected/(np.sqrt(v_t_corrected) + group['eps'] + group['lr'] * p.data)
+
+                # Updating params
+                p_data_t = p.data
+                p.data -= alpha * m_t/(np.sqrt(v_t) + group['eps']) + group['lr'] * p.data
+
+                # Updating params using weight decay
+                # According to Ilya Loshchilov et al., Adam with decouple weight decay without a scheduled lr is simply adding lambda * theta_{t-1}
+                # p.data -= alpha * m_t/(np.sqrt(v_t) + group['eps']) + group['lr'] * p.data
+                # p.data += group['weight_decay'] * group['lr']
+                # p.data += group['weight_decay'] * p_data_t
+                # p.data -= group['lr'] * (p.data + group['weight_decay'])
+                # p.data -= group['weight_decay'] * (alpha * m_t/(np.sqrt(v_t) + group['eps']) + group['lr'] * p.data)
+
+                return p.data
                 #raise NotImplementedError
-
-
         return loss
+
+
+    # when p.data -= alpha * m_t/(np.sqrt(v_t) + group['eps']) + group['lr'] * p.data for first update
+    # tensor([[ 0.5548,  0.8667,  0.0729],
+    #         [-0.4472, -0.2951, -0.2717]])
+    # tensor([[ 0.5549,  0.6704,  0.3818],
+    #         [-0.3780, -0.3042, -0.1660]])
+
+    # when p.data -= alpha * m_t/(np.sqrt(v_t) + group['eps']) + group['lr'] * p_data_t
+    # tensor([[ 0.5548,  0.8667,  0.0729],
+    #         [-0.4472, -0.2951, -0.2717]])
+    # tensor([[ 0.6213,  0.7386,  0.4485],
+    #         [-0.4267, -0.3530, -0.2152]])
+
+    # when p.data -= group['lr'] * (p.data + group['weight_decay'])
+    # tensor([[ 0.5548,  0.8667,  0.0729],
+    #         [-0.4472, -0.2951, -0.2717]])
+    # tensor([[ 0.5543,  0.6698,  0.3811],
+    #         [-0.3779, -0.3041, -0.1659]])
+
